c2e585790f30e64559738a1cd294adf7ea018ddf
Zoran Jovanov <stefan.jovanov98@gmail.com>
2019-05-14 00:05:32 +0200

    Prv commit od doma

---
+++ Lab3/Controllers/FriendController.cs
@@ -0,0 +1,65 @@
+﻿using Lab3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class FriendController : Controller
+    {
+        static List<FriendModel> lista = new List<FriendModel>();
+        // GET: Friend
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult All()
+        {
+            return View(lista);
+        }
+
+        public ActionResult Add()
+        {
+            FriendModel prijatel = new FriendModel();
+            return View(prijatel);
+        }
+
+        public ActionResult AddNewFriend(FriendModel prijatel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Add", prijatel);
+            }
+            lista.Add(prijatel);
+            return View("All", lista);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var prijatel = lista.Single(m => m.id == id);
+            return View(prijatel);
+        }
+
+        public ActionResult EditFriend(FriendModel prijatel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Edit", prijatel.id);
+            }
+            FriendModel pomoshen = lista.Single(m => m.id == prijatel.id);
+            pomoshen.id = prijatel.id;
+            pomoshen.ime = prijatel.ime;
+            pomoshen.mestoNaZiveenje = prijatel.mestoNaZiveenje;
+            return View("All", lista);
+        }
+
+        public ActionResult DeleteFriend(int id)
+        {
+            lista.RemoveAt(id);
+            return View("All", lista);
+        }
+    }
+}
---
+++ Lab3/Models/FriendModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab3.Models
+{
+    public class FriendModel
+    {
+        [Required]
+        [Range(0, 200, ErrorMessage = "ID treba da e broj pomegju 0 i 200")]
+        [Display (Name ="Friend ID")]
+        public int id { set; get; }
+        [Required]
+        [Display(Name = "Friend Name")]
+        public String ime { get; set; }
+        [Required]
+        [Display(Name = "Place")]
+        public String mestoNaZiveenje { get; set; }
+    }
+}
